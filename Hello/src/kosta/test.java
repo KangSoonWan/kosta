b0162af399688a14ae083a1ebe337c2d5b747fae
KangSoonWan <curious906@naver.com>
2019-01-09 12:36:58 +0900

    ㅁ

--- Hello/src/kosta/test.java
+++ Hello/src/kosta/test.java
@@ -3,15 +3,8 @@ package kosta;
 public class test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-System.out.println("MergeTool");
-System.out.println("Master");
-=======
-		System.out.println("변경");
-		System.out.println("변경");
-		System.out.println("변경");
-
->>>>>>> branch 'sever' of https://github.com/KangSoonWan/kosta.git
+		System.out.println("MergeTool");
+		System.out.println("연습하는겁니당");
 	}
 
 }
